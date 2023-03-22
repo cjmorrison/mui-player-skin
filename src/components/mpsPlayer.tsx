9b7fa51d54761d9ee4f09a3b436eeda1f5f9bbab
cjmorrison <cj@cjmorrison>
2023-03-21 22:25:13 -0400

    use drawer mode added

--- src/components/mpsPlayer.tsx
+++ src/components/mpsPlayer.tsx
@@ -6,20 +6,26 @@ import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
+import Tooltip from "@mui/material/Tooltip";
+import Drawer from "@mui/material/Drawer";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import SpeedIcon from "@mui/icons-material/Speed";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import VolumeDownIcon from "@mui/icons-material/VolumeDown";
 import PauseIcon from "@mui/icons-material/Pause";
+import LyricsIcon from "@mui/icons-material/Lyrics";
 
 interface PropType {
   src?: string;
+  useDrawer?: boolean;
 }
 interface StateType {
   audioPlaying: boolean;
   audioPos: number;
   volume: number;
+  drawerStatus: boolean;
   speedMenuOpen: boolean;
   volMenuOpen: boolean;
   speedMenuAnchorEl: null | HTMLElement;
@@ -33,10 +39,24 @@ class MpsPlayer extends React.Component<PropType, StateType> {
     audioPlaying: false,
     audioPos: 0,
     volume: 100,
+    drawerStatus: false,
     speedMenuOpen: false,
     volMenuOpen: false,
     speedMenuAnchorEl: null,
   };
+
+  theme = createTheme({
+    palette: {
+      primary: {
+        main: "#1b5e20",
+        contrastText: "#fff",
+      },
+      secondary: {
+        main: "#2979ff",
+        contrastText: "#000",
+      },
+    },
+  });
 
   constructor(props) {
     super(props);
@@ -56,6 +76,12 @@ class MpsPlayer extends React.Component<PropType, StateType> {
       this.usingAudioSrc = this.props.src;
     } else {
       this.usingAudioSrc = "./sampleAudio.mp3";
+    }
+
+    if (this.props.useDrawer) {
+      this.setState({
+        volMenuOpen: true,
+      });
     }
   };
 
@@ -156,6 +182,19 @@ class MpsPlayer extends React.Component<PropType, StateType> {
     this.closeSpeedMenu();
   };
 
+  toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      this.setState({ drawerStatus: open });
+    };
+
   render() {
     let audioPos = 0;
 
@@ -163,51 +202,198 @@ class MpsPlayer extends React.Component<PropType, StateType> {
       audioPos = (this.state.audioPos / this.ref.current.duration) * 100;
     }
 
+    let audSliderClass = "mps_slider";
+    if (this.props.useDrawer) {
+      audSliderClass = "mps_slider useDrawer";
+    }
+
+    const buildContainer = () => {
+      return (
+        <Box className="mps_container">
+          {displayPlayFab()}
+          <Slider
+            id="mps_audioPos"
+            className={audSliderClass}
+            aria-label="Audio posistion"
+            defaultValue={0}
+            value={audioPos}
+            onChange={this.setAudioPos}
+            //   color="secondary"
+          />
+
+          {displayVolumeToggle()}
+
+          <Tooltip title="Playback Speed" placement="bottom">
+            <Fab
+              color="primary"
+              className="mps_fab"
+              aria-label="adjust playback speed"
+              aria-controls={
+                this.state.speedMenuOpen ? "playbackSpeed" : undefined
+              }
+              aria-haspopup="true"
+              aria-expanded={this.state.speedMenuOpen ? "true" : undefined}
+              onClick={this.openSpeedMenu}
+              sx={{ mr: 1 }}
+            >
+              <SpeedIcon></SpeedIcon>
+            </Fab>
+          </Tooltip>
+
+          <Menu
+            id="playbackSpeed-menu"
+            anchorEl={this.state.speedMenuAnchorEl}
+            open={this.state.speedMenuOpen}
+            onClose={this.closeSpeedMenu}
+            MenuListProps={{
+              "aria-labelledby": "playbackSpeed-button",
+            }}
+          >
+            <MenuItem
+              onClick={() => {
+                this.setAudioPBS(0.25);
+              }}
+            >
+              0.25
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                this.setAudioPBS(0.5);
+              }}
+            >
+              0.50
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                this.setAudioPBS(0.75);
+              }}
+            >
+              0.75
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                this.setAudioPBS(1);
+              }}
+            >
+              Normal
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                this.setAudioPBS(1.25);
+              }}
+            >
+              1.25
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                this.setAudioPBS(1.5);
+              }}
+            >
+              1.50
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                this.setAudioPBS(1.75);
+              }}
+            >
+              1.75
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                this.setAudioPBS(2);
+              }}
+            >
+              x2
+            </MenuItem>
+          </Menu>
+
+          {displayVolume()}
+          <audio
+            ref={this.ref}
+            className="mps_audTag"
+            src={this.usingAudioSrc}
+          ></audio>
+        </Box>
+      );
+    };
+
     const displayPlayFab = () => {
       if (this.state.audioPlaying) {
         return (
-          <Fab
-            className="mps_fab"
-            color="primary"
-            aria-label="stop audio"
-            onClick={this.togglePlayAudio}
-            sx={{ mr: 1 }}
-          >
-            <PauseIcon></PauseIcon>
-          </Fab>
+          <Tooltip title="Pause" placement="bottom">
+            <Fab
+              className="mps_fab"
+              color="primary"
+              aria-label="stop audio"
+              onClick={this.togglePlayAudio}
+              sx={{ mr: 1 }}
+            >
+              <PauseIcon></PauseIcon>
+            </Fab>
+          </Tooltip>
         );
       }
 
       return (
-        <Fab
-          className="mps_fab"
-          color="primary"
-          aria-label="play audio"
-          onClick={this.togglePlayAudio}
-          sx={{ mr: 1 }}
-        >
-          <PlayArrowIcon></PlayArrowIcon>
-        </Fab>
+        <Tooltip title="Play" placement="bottom">
+          <Fab
+            className="mps_fab"
+            color="primary"
+            aria-label="play audio"
+            onClick={this.togglePlayAudio}
+            sx={{ mr: 1 }}
+          >
+            <PlayArrowIcon></PlayArrowIcon>
+          </Fab>
+        </Tooltip>
       );
     };
 
+    const displayVolumeToggle = () => {
+      if (!this.props.useDrawer) {
+        return (
+          <Tooltip title="Volume Settings" placement="bottom">
+            <Fab
+              color="primary"
+              className="mps_fab"
+              aria-label="open volume"
+              aria-controls={this.state.volMenuOpen ? "volume-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={this.state.volMenuOpen ? "true" : undefined}
+              onClick={this.toggleVolumeDisplay}
+              sx={{ mr: 1 }}
+            >
+              <VolumeUpIcon></VolumeUpIcon>
+            </Fab>
+          </Tooltip>
+        );
+      } else {
+        return "";
+      }
+    };
+
     const displayVolume = () => {
+      let volSliderClass = "";
+      if (this.props.useDrawer) {
+        volSliderClass = "useDrawer";
+      }
+
       if (this.state.volMenuOpen) {
         return (
-          <Box id="mps_volume">
+          <Box id="mps_volume" className={volSliderClass}>
             <Stack
               spacing={2}
               direction="row"
               sx={{ mb: 1 }}
               alignItems="center"
             >
-              <VolumeDownIcon />
+              <VolumeDownIcon color="primary" />
               <Slider
                 aria-label="Volume"
                 value={this.state.volume}
                 onChange={this.setVolume}
               />
-              <VolumeUpIcon />
+              <VolumeUpIcon color="primary" />
             </Stack>
           </Box>
         );
@@ -216,120 +402,35 @@ class MpsPlayer extends React.Component<PropType, StateType> {
       }
     };
 
-    return (
-      <Box className="mps_container">
-        {displayPlayFab()}
-        <Slider
-          id="mps_audioPos"
-          className="mps_slider"
-          aria-label="Audio posistion"
-          defaultValue={0}
-          value={audioPos}
-          onChange={this.setAudioPos}
-          //   color="secondary"
-        />
-
-        <Fab
-          color="primary"
-          className="mps_fab"
-          aria-label="open volume"
-          aria-controls={this.state.volMenuOpen ? "volume-menu" : undefined}
-          aria-haspopup="true"
-          aria-expanded={this.state.volMenuOpen ? "true" : undefined}
-          onClick={this.toggleVolumeDisplay}
-          sx={{ mr: 1 }}
-        >
-          <VolumeUpIcon></VolumeUpIcon>
-        </Fab>
-
-        <Fab
-          color="primary"
-          className="mps_fab"
-          aria-label="adjust playback speed"
-          aria-controls={this.state.speedMenuOpen ? "playbackSpeed" : undefined}
-          aria-haspopup="true"
-          aria-expanded={this.state.speedMenuOpen ? "true" : undefined}
-          onClick={this.openSpeedMenu}
-          sx={{ mr: 1 }}
-        >
-          <SpeedIcon></SpeedIcon>
-        </Fab>
-
-        <Menu
-          id="playbackSpeed-menu"
-          anchorEl={this.state.speedMenuAnchorEl}
-          open={this.state.speedMenuOpen}
-          onClose={this.closeSpeedMenu}
-          MenuListProps={{
-            "aria-labelledby": "playbackSpeed-button",
-          }}
-        >
-          <MenuItem
-            onClick={() => {
-              this.setAudioPBS(0.25);
-            }}
-          >
-            0.25
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-              this.setAudioPBS(0.5);
-            }}
-          >
-            0.50
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-              this.setAudioPBS(0.75);
-            }}
-          >
-            0.75
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-              this.setAudioPBS(1);
-            }}
-          >
-            Normal
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-              this.setAudioPBS(1.25);
-            }}
-          >
-            1.25
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-              this.setAudioPBS(1.5);
-            }}
-          >
-            1.50
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-              this.setAudioPBS(1.75);
-            }}
-          >
-            1.75
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-              this.setAudioPBS(2);
-            }}
-          >
-            x2
-          </MenuItem>
-        </Menu>
-
-        {displayVolume()}
-        <audio
-          ref={this.ref}
-          className="mps_audTag"
-          src={this.usingAudioSrc}
-        ></audio>
-      </Box>
-    );
+    if (!this.props.useDrawer) {
+      return <div>{buildContainer()}</div>;
+    } else {
+      return (
+        <Box>
+          <ThemeProvider theme={this.theme}>
+            <Tooltip title="Audio Settings" placement="bottom-end">
+              <Fab
+                className="mps_fab"
+                size="medium"
+                color="primary"
+                aria-label="play audio"
+                onClick={this.toggleDrawer(true)}
+                sx={{ mr: 1 }}
+              >
+                <LyricsIcon></LyricsIcon>
+              </Fab>
+            </Tooltip>
+            <Drawer
+              anchor="top"
+              open={this.state.drawerStatus}
+              onClose={this.toggleDrawer(false)}
+            >
+              {buildContainer()}
+            </Drawer>
+          </ThemeProvider>
+        </Box>
+      );
+    }
   }
 }
 
